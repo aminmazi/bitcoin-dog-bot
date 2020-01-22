012710ea9457c245bfc1fd28b98724d244fdc5ce
Amin Mazinani <amin.mazinani@redacreltd.com>
2020-01-22 23:06:31 +0330

    fix: default value for cache config

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,7 +2,7 @@ import envalid from "envalid";
 
 const env = envalid.cleanEnv(process.env, {
   BOT_TOKEN: envalid.str(),
-  CACHE_INTERVAL: envalid.num(),
+  CACHE_INTERVAL: envalid.num({ default: 30 }),
 });
 
 export default env;
